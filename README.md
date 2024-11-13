79de2b2810efabfca71f2c4e6d838847b3301c21
Bella Pfeiffer <bellap@Bellas-MacBook-Pro.local>
2024-11-13 13:12:32 -0500

    updated readme; increased commenting on mfuzz

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # legend_screen
+
 Analysis of cell surface marker screening assay via PCA + k-means (R- factoextra), and time-based clustering (R - mfuzz). Antibody names removed for privacy purposes. HUGE thank you to KH for all her help.
+
+This repository demonstrates subject matter proficiency in the following areas:
+1. Differential Expression Analysis
+2. Data Wrangling
+3. Data Visualization and Exploration
+4. Clustering, Dimensionality Reduction, and Pattern Recognition
+
+This repository demonstrates the following skills:
+1. Self-directed Learning
+2. Understanding and Application of existing packages to relevant datasets
--- mfuzz_legendscreen_bella_v1.R
+++ mfuzz_legendscreen_bella_v1.R
@@ -1,4 +1,4 @@
-# Mfuzz: using noise-robust soft clustering of omics time-series data
+# mfuzz: using noise-robust soft clustering of omics time-series data
 # in our case, this package allowed us to visualize how different cell 
 # surface markers change in their expression over time; this is really
 # helpful since we have very distinct timepoints.
@@ -16,16 +16,21 @@ library(Mfuzz) # soft clustering of omics time-series data
 library(dplyr) # data manipulation
 library(tidyr) # tidying data + more manipulation
 
-
+# loading in data
 mfuzz_mat <- read.csv("/Users/bellapfeiffer/Dropbox (Harvard University)/Circle Tx/R Scripts/LegendScreen Analysis BP/timepoint analysis.csv", header = TRUE, row.names = 1, stringsAsFactors = FALSE)
+# converting to expressionset data type
 eset <- ExpressionSet(assayData = as.matrix(mfuzz_mat))
 
-# conduct analysis
+# conduct mfuzz analysis
 c <- mfuzz(eset, c = 8, m = 1.25)  # c = number of clusters, m = fuzziness parameter
 # after more research: no need to do hard k-means; does not assist in comparison
-membership_values <- c$membership 
-max_membership <- apply(membership_values, 1, which.max)
-write.table(max_membership, file = "mfuzz.cluster.membership.v1.txt")
+
+# output which markers are in which cluster as .txt file (for cross reference with PCA)
+membership_values <- c$membership #only include membership values
+max_membership <- apply(membership_values, 1, which.max) 
+write.table(max_membership, file = "mfuzz.cluster.membership.v1.txt") # save which 
 
 # visualize as clusters - set ylim to be 100
 mfuzz.plot2(eset, c, ylim=c(0,100), mfrow = c(2, 4), new.window = FALSE)
+
+
